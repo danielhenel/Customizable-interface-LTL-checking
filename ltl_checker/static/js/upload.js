742952ed4287d31a2720fb71460c40cfd30c3cec
Daniel Henel <daniel-henel@protonmail.com>
2022-11-18 18:35:10 +0100

    change_cloud_color

--- ltl_checker/static/js/upload.js
+++ ltl_checker/static/js/upload.js
@@ -1,8 +1,24 @@
-function msgprint() {  
+var file_field = document.getElementById('file_field')
+file_field.onchange = change_cloud_color
 
+function change_cloud_color(){
     var image = document.getElementById("cloud-image");
-    image.src = "./static/images/cloud-green.png";
-    
-}  
-var refButton = document.getElementById("cloud-image");
-refButton.onmouseleave = msgprint
+
+    // get file name
+    var fullPath = document.getElementById('file_field').value;
+    if (fullPath) {
+        var startIndex = (fullPath.indexOf('\\') >= 0 ? fullPath.lastIndexOf('\\') : fullPath.lastIndexOf('/'));
+        var filename = fullPath.substring(startIndex);
+        if (filename.indexOf('\\') === 0 || filename.indexOf('/') === 0) {
+            filename = filename.substring(1);
+        }
+    }
+
+    format = filename.slice(-3)
+
+    if (format == "csv" || format=="xes"){
+        image.src = "./static/images/cloud-green.png";
+    } else {
+        image.src = "./static/images/cloud-red.png";
+    }
+}
