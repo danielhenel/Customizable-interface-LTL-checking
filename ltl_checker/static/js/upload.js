7be4490df57e039269d975d141a955e6ff2641c0
Daniel Henel <daniel-henel@protonmail.com>
2022-11-18 15:06:08 +0100

    upload screen script - initial version

---
+++ ltl_checker/static/js/upload.js
@@ -0,0 +1,8 @@
+function msgprint() {  
+
+    var image = document.getElementById("cloud-image");
+    image.src = "./static/images/cloud-green.png";
+    
+}  
+var refButton = document.getElementById("cloud-image");
+refButton.onmouseleave = msgprint
