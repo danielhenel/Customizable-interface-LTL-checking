ba91ffca8a0685d19151c5920bac571188eefbe0
Daniel Henel <daniel.henel@rwth-aachen.de>
2022-11-25 19:14:39 +0100

    Merge branch 'dev2'

--- ltl_checker/static/js/upload.js
+++ ltl_checker/static/js/upload.js
@@ -27,12 +27,53 @@ function changeCloudColor(){
             form = document.getElementById("uploadFileForm")
             form.submit()
         }
+        texttoshow = "the text related to first box should be displayed";
+       //****//
+       //remove parts of text
+       var h = document.getElementsByTagName('h2');
+        for (var i = h.length; i--; ) {
+            h[i].style.display = 'none';
+        }       
+        document.getElementById("fail").style.visibility = "hidden";
+
+        //hide upload button DOES NOT WORK
+        document.getElementById('fileField').style.display = 'none';
+
         
+        //display file name
+        success.innerText += ' File successfully uploaded ';
+        var fullPath = document.getElementById('fileField').value;
+            if (fullPath) {
+                var startIndex = (fullPath.indexOf('\\') >= 0 ? fullPath.lastIndexOf('\\') : fullPath.lastIndexOf('/'));
+                var filename = fullPath.substring(startIndex);
+                    if (filename.indexOf('\\') === 0 || filename.indexOf('/') === 0) {
+                        filename = filename.substring(1);
+                     }
+                    
+                    filena.innerText += filename ;
+            }
+
+        // Reload button
+        let btn = document.createElement("rbutton");
+        btn.innerHTML =  '<button id="rbutton" name="btn"> <img src="static/images/reload.png"  width="45" height="45" />   '; 
+        btn.className = "rbutton";
+
+        btn.addEventListener("click", function () {
+            window.location.reload();
+        });
+        
+        document.body.appendChild(btn);
+
+        
+
     } else {
         // red color when the file is in the wrong format
         image.src = "./static/images/cloud-red.png";
         document.getElementById("nextButton").style.visibility = "hidden";
-        alert("The file should be in .csv or .xes format")
+        //alert("The file should be in .csv or .xes format")
+        //text for wrong file
+        fail.innerText += ' \n UPLOAD FAILED ! \n The file must be either *.csv or *.xes" ';
+         // same as: document.getElementById('fail').innerText += 'textexample'
     }
 }
 
