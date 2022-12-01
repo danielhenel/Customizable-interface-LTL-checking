f798060b12cc9c917a627e3a1a31cc3f803794ba
unknown <ugo.detaille@rwth-aachen.de>
2022-12-01 14:19:51 +0100

    update convertColumns function in main-py

--- ltl_checker/main.py
+++ ltl_checker/main.py
@@ -36,7 +36,29 @@ def convertInput():
         raw_log = pm4py.read_xes(file)
         raw_log = pm4py.convert_to_dataframe(raw_log)
         return raw_log
+
+
+
+def renameColumns(columns_to_drop, columns_to_rename):
+    # we define mandatory_columns as the columns the user cannot drop and therefore
+    #ignore all selections of such columns
+    global data 
+    global mandatory_columns 
+    mandatory_columns = ["Case ID", "Activity Name", "Time Stamp" , "Resource"]
+    for column in columns_to_drop:
+        if(column in mandatory_columns): 
+            pass
+        else:
+            data = data.drop(column, axis=1)
+    #rename the dataframe by handing the rename function a dictionary
+    data = data.rename(columns=columns_to_rename, inplace=True)
+        
+
+        
+
     
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(debug=True, host='0.0.0.0', port=port)
+
+    
