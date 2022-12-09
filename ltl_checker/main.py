286526203cb897f2fce07bfbfe94b8bf0d3225b1
Daniel Henel <daniel.henel@rwth-aachen.de>
2022-12-09 22:49:07 +0100

    fix bugs with the calc_result function + results

--- ltl_checker/main.py
+++ ltl_checker/main.py
@@ -40,27 +40,31 @@ def afterColumnsSelection():
     drop_cols = message[0]
     new_names = message[1]
     renameColumns(drop_cols,new_names)
-    return data.to_json()
+    return "success"
 
 @app.route('/selectFilters')
 def loadSelectFiltersPage():
-    return render_template('filters-selection.html')
+    return render_template('filters-selection.html',data=getActivities())
 
 @app.route('/selectFilters/message', methods = ['POST'])
 def afterFilterSelection():
     global expression
     global terms_dict
+    global result
     message = json.loads(request.data)
     terms_dict = message[0]
     expression = message[1]
     expression = sympify(expression,_clash1)
     calc_result()
-    print(result.head(5))
     return 
 
 @app.route('/selectColumns2',methods = ['POST','GET'])
 def goBackToSelectColumns(): 
     return render_template('columns-selection.html',data=data.head(5).to_json())
+
+@app.route('/results',methods = ['POST','GET'])
+def results(): 
+    return render_template('results.html',data=json.dumps([data.head(6).to_json(),len(data.index)]))
 
 @app.route('/selectColumns',methods = ['POST','GET'])
 def upload():
@@ -69,6 +73,7 @@ def upload():
         global file
         file = request.files['file']
         data = convertInput()
+        data.drop_duplicates(list(data.columns))
         return render_template('columns-selection.html',data=data.head(5).to_json())
     except pd.errors.EmptyDataError: 
         return render_template('error.html', message = 'Your data is empty!!!')
@@ -110,16 +115,19 @@ def renameColumns(columns_to_drop, columns_to_rename):
 def simplifyExpression(expr) -> list: #this function returns a list of all the conjunctively connected clauses in the filter expression
     #convert expression to CNF
     cnf = sp.to_cnf(expr, True)
-    # seperate different Clauses in CNF
-    clauses = cnf.args
-    clause_list = tupleToList(clauses)
-    #convert clauses in list to list of literals
-    for i in range(0,len(clause_list)):
-        if(len(clause_list[i].args) != 0):
-            clause_list[i] = tupleToList(clause_list[i].args)
-        else:
-            clause_list[i] = [clause_list[i]]
-    return clause_list
+    if len(cnf.args) != 0:
+        # seperate different Clauses in CNF
+        clauses = cnf.args
+        clause_list = tupleToList(clauses)
+        #convert clauses in list to list of literals
+        for i in range(0,len(clause_list)):
+            if(len(clause_list[i].args) != 0):
+                clause_list[i] = tupleToList(clause_list[i].args)
+            else:
+                clause_list[i] = [clause_list[i]]
+        return clause_list
+    else:
+        return [[cnf]]
 
 def tupleToList(tuple) -> list: 
     retList = []
@@ -129,8 +137,9 @@ def tupleToList(tuple) -> list:
 
     
 
-def getActivities(dataframe): 
-    return dataframe['concept:name'].unique()
+def getActivities(): 
+    global data
+    return (data['concept:name'].unique()).tolist()
 
 def calc_result() -> pd.DataFrame:
     global data
@@ -142,32 +151,32 @@ def calc_result() -> pd.DataFrame:
     for terms in simplifyExpression(expression):
         j = 0
         temp = None
-
         for term in terms:
-            filterType = terms_dict[term][0]
+            key = str(term)
+            filterType = terms_dict[key][0]
 
             if j == 0:
                 if filterType == 'four_eyes_principle': 
-                    temp = four_eyes_principle(data,terms_dict[term][1])
+                    temp = four_eyes_principle(data,terms_dict[key][1])
                 elif filterType == 'eventually_follows_2': 
-                    temp = eventually_follows_2(data,terms_dict[term][1])
+                    temp = eventually_follows_2(data,terms_dict[key][1])
                 elif filterType == 'eventually_follows_3': 
-                    temp = eventually_follows_3(data,terms_dict[term][1])
+                    temp = eventually_follows_3(data,terms_dict[key][1])
                 elif filterType == 'eventually_follows_4':
-                    temp = eventually_follows_4(data,terms_dict[term][1])
+                    temp = eventually_follows_4(data,terms_dict[key][1])
                 elif filterType == 'attribute_value_different_persons':
-                    temp = attribute_value_different_persons(data,terms_dict[term][1])
+                    temp = attribute_value_different_persons(data,terms_dict[key][1])
             else:
                 if filterType == 'four_eyes_principle': 
-                    temp = df_union(temp,four_eyes_principle(data,terms_dict[term][1]))
+                    temp = df_union(temp,four_eyes_principle(data,terms_dict[key][1]))
                 elif filterType == 'eventually_follows_2': 
-                    temp = df_union(temp,eventually_follows_2(data,terms_dict[term][1]))
+                    temp = df_union(temp,eventually_follows_2(data,terms_dict[key][1]))
                 elif filterType == 'eventually_follows_3': 
-                    temp = df_union(temp,eventually_follows_3(data,terms_dict[term][1]))
+                    temp = df_union(temp,eventually_follows_3(data,terms_dict[key][1]))
                 elif filterType == 'eventually_follows_4':
-                    temp = df_union(temp,eventually_follows_4(data,terms_dict[term][1]))
+                    temp = df_union(temp,eventually_follows_4(data,terms_dict[key][1]))
                 elif filterType == 'attribute_value_different_persons':
-                    temp = df_union(temp,attribute_value_different_persons(data,terms_dict[term][1]))
+                    temp = df_union(temp,attribute_value_different_persons(data,terms_dict[key][1]))
         
             j += 1
         
@@ -178,6 +187,9 @@ def calc_result() -> pd.DataFrame:
         
         i+=1
 
+    data.rename(columns={"case:concept:name" : "Case ID", "concept:name" : "Activity Name", "time:timestamp" : "Time Stamp" , "org:resource" : "Resource"}, inplace = True)   
+
+
 
 def df_intersection(A, B):
     return pd.merge(A, B, how ='inner', on =list(A.columns)).reset_index().drop(columns=["index"])
@@ -185,23 +197,23 @@ def df_intersection(A, B):
 def df_union(A,B):
     return pd.concat([A,B]).drop_duplicates(list(A.columns)).reset_index().drop(columns=["index"])
 
-def four_eyes_principle(activites,df):
+def four_eyes_principle(df,activites):
     filtered_log = ltl.ltl_checker.four_eyes_principle(df,*activites)
     return filtered_log
 
-def eventually_follows_2(activities,df): 
+def eventually_follows_2(df,activities): 
     filtered_log = ltl.ltl_checker.A_eventually_B(df,*activities)
     return filtered_log
 
-def eventually_follows_3(activities,df):
+def eventually_follows_3(df,activities):
     filtered_log = ltl.ltl_checker.A_eventually_B_eventually_C(df,*activities)
     return filtered_log
 
-def eventually_follows_4(activities,df):
+def eventually_follows_4(df,activities):
     filtered_log = ltl.ltl_checker.A_eventually_B_eventually_C_eventually_D(df,*activities)
     return filtered_log
 
-def attribute_value_different_persons(activities, df):
+def attribute_value_different_persons(df,activities):
     filtered_log = ltl.ltl_checker.attr_value_different_persons(df, *activities)
     return filtered_log
 
