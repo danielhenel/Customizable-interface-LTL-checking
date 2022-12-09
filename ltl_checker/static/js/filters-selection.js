3a743b4a2182bee6d73eeeb456d92bccb502031c
Daniel Henel <daniel.henel@rwth-aachen.de>
2022-12-09 19:18:07 +0100

    create check_formula() function + fix some bugs

--- ltl_checker/static/js/filters-selection.js
+++ ltl_checker/static/js/filters-selection.js
@@ -7,11 +7,18 @@ emptyFilter = false
 refresh = false
 attributes = ["A","B","C","D"]
 
-document.getElementById("nextButton").style.visibility = "visible";
+document.getElementById("previousButton").style.visibility = "visible";
+document.getElementById("previousButton").onclick = function(){window.location.replace('/selectColumns2')}
+document.getElementById("nextButton").onclick = function(){
+    var message = prepare_message()
+    fetch('/selectFilters/message', {
+    method: 'POST',
+    body: message
+    })
+    window.location.replace('/results')
+}
 
-document.getElementById("nextButton").onclick = function(){prepare_message()}
 createFirstRow()
-
 
 function createFirstRow() {
 
@@ -80,6 +87,9 @@ function createNewRowBelow(previousRow){
 
     body.insertBefore(row, previousRow.nextSibling);
     rowCounter += 1
+
+    filterSelected(selectFilter)
+    check_formula()
 }
 
 
@@ -89,7 +99,8 @@ function createSelectOptionList (){
     selectList.classList.add("selectList")
     selectList.classList.add("selectOption")
     //optionSelected(selectList)
-    selectList.onchange = function(){optionSelected(selectList)}
+    selectList.onchange = function(){optionSelected(selectList) 
+        check_formula()}
 
     if(rowCounter == 1){
         var item = document.createElement('option')
@@ -112,22 +123,22 @@ function createSelectOptionList (){
         }
         else{
             var item = document.createElement('option')
-            item.value = "OR"
+            item.value = "|"
             item.innerText = "OR"
             selectList.appendChild(item)
         
             item = document.createElement('option')
-            item.value = "AND"
+            item.value = "&"
             item.innerText = "AND"
             selectList.appendChild(item)
     
             item = document.createElement('option')
-            item.value = "AND ("
+            item.value = "& ("
             item.innerText = "AND ("
             selectList.appendChild(item)
         
             item = document.createElement('option')
-            item.value = "OR ("
+            item.value = "| ("
             item.innerText = "OR ("
             selectList.appendChild(item)
         }
@@ -148,8 +159,8 @@ function createSelectFilterList(){
     selectList.style.width = "100%"
     selectList.classList.add("selectList")
     selectList.classList.add("selectFilter")
-    //filterSelected(selectList)
-    selectList.onchange = function(){filterSelected(selectList)}
+    selectList.onchange = function(){filterSelected(selectList)
+        check_formula()}
 
     var item = document.createElement('option')
     item.value = "four_eyes_principle"
@@ -182,7 +193,7 @@ function createSelectFilterList(){
         item.innerText = ""
         selectList.appendChild(item)
     }
-
+    
     return selectList
 }
 
@@ -193,7 +204,7 @@ function createSelectAttributeList(attributes){
     selectList.classList.add("selectList")
     selectList.classList.add("selectFristAttribute")
 
-  //  selectList.onchange = function(){firstAttributeSelected(selectList)}
+    selectList.onchange = function(){check_formula()}
     
     attributes.forEach(function(attribute){
 
@@ -211,6 +222,7 @@ function createAddButton(previousRow){
     button.classList.add("addButton")
     button.onclick = function(){createNewRowBelow(previousRow)}
     var img = document.createElement("img")
+    img.classList.add("addButtonIMG")
     img.src = "static/images/plus.png"
     button.appendChild(img)
     return button
@@ -220,11 +232,21 @@ function createDeleteButton(row){
     var button = document.createElement("button")
     button.classList.add("deleteButton")
     button.onclick = function(){
-        parent = row.parentElement
+        var parent = row.parentElement
+        var fields = row.children
+        var option = fields[0].firstChild.value
+        if(option == ")"){
+            rightBracketCounter --
+        }
+        else if(option == "(" || option == "| (" || option == "| ("){
+            leftBracketCounter --
+        }
         parent.removeChild(row)
-        rowCounter -= 1}
+        rowCounter -= 1
+    }
     var img = document.createElement("img")
     img.src = "static/images/bin.png"
+    img.classList.add("deleteButtonIMG")
     button.appendChild(img)
     return button
 }
@@ -254,7 +276,7 @@ function optionSelected(selectList){
         }
         emptyFilter = false
     }
-    else if (selected=="(" || selected =="AND (" || selected == "OR ("){
+    else if (selected=="(" || selected =="& (" || selected == "| ("){
         leftBracketCounter += 1
         //add empty
         var item = document.createElement('option')
@@ -334,14 +356,19 @@ function prepare_message(){
         var filter = null
         var attributes = []
         var key = ""
+        var rightBracket = false
         for(var j=0; j<6; j++){
             
             var select = fields[j].firstChild
             
             if(j==0){
                 option = select.value
-                if(option!="empty"){``
+                if(option!="empty"){
                 expression = expression.concat(" ", option)
+                if(option == ")"){
+                    rightBracket = true
+                    break
+                }
                 }
             }
             else if(j==1){
@@ -357,6 +384,7 @@ function prepare_message(){
                 }
             }
         }
+        if(rightBracket){continue}
         // check if the term exists
         if(key in dictionary){
             var term = terms[key]
@@ -384,5 +412,55 @@ function prepare_message(){
     )
 
     var message = [result_dict,expression]
-    return message
+    return JSON.stringify(message)
+}
+
+function check_formula(){
+    
+    var rows = body.children
+    var right = 0
+    var left = 0
+    var different = true
+
+    for(var i=1; i<rows.length; i++){
+        var fields = rows[i].children
+        var attributes = []
+        for(var j=0; j<6; j++){
+            var select = fields[j].firstChild
+            
+            if(j==0){
+                if(select.value == "(" || select.value == "| (" || select.value == "& ("){
+                    left ++
+                }
+                else if(select.value == ")"){
+                    right ++
+                }
+            }
+            else if (j>1){
+                if(fields[j].style.visibility != "hidden")
+                {
+                    var attribute = select.value
+                    attributes.push(attribute)
+                }
+            }
+
+            for(var k=0; k<attributes.length; k++)
+            {
+                for(var l=0; l<attributes.length; l++){
+                    if(k!=l){
+                        if(attributes[k] == attributes[l]){
+                            different = false
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    if(left==right && different){
+        document.getElementById("nextButton").style.visibility = "visible";
+    }
+    else{
+        document.getElementById("nextButton").style.visibility = "hidden";
+    }
 }
