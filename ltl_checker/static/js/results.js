d24655071510015902ea4678ed2b37c2910090b0
Daniel Henel <daniel.henel@rwth-aachen.de>
2022-12-23 07:45:36 +0100

    show message after clicking a row

--- ltl_checker/static/js/results.js
+++ ltl_checker/static/js/results.js
@@ -8,7 +8,6 @@ function loadDataFromFile(data){
     deviations_description = data[3]
     messages = deviations_description[0]
     highlight_rows = deviations_description[1]
-    alert(highlight_rows)
     var number = document.getElementById("number")
     number.insertAdjacentText('beforeend', currentPage + "/" + maxPage)
     data = JSON.parse(data[0])
@@ -45,9 +44,69 @@ function loadDataFromFile(data){
             }
         }
         var bodyRow = document.createElement('tr');
+        bodyRow.onmouseenter = function(){this.style.backgroundColor = "white"}
+        bodyRow.onmouseleave = function(){this.style.backgroundColor = "#c9b6b6"}
+        bodyRow.i = i
+        if(highlight_rows.includes(i)){
+            bodyRow.onclick = function(){
+                var ul = document.createElement("ul")
+                messages.forEach(element => {
+                    msg = element[0]
+                    rows = element[1]
+                    if(rows.includes(this.i)){
+                        var li = document.createElement("li")
+                        li.textContent = msg
+                        ul.appendChild(li)
+                    }});
+                    
+                    var popup_html = `
+                    <div id="popup_temp" class="modal fade" role="dialog">
+                      <div class="modal-dialog modal-dialog-centered">
+                              <div class="modal-content">
+                                <div class="modal-header">
+                                    <p class="modal-title title-style">Deviations found</p>
+                                </div>
+                                <div class="modal-body">
+                                    <p id="dev">
+                                    </p>
+                                </div>
+                                <div class="modal-footer">
+                                  <button id="close_button" type="button" class="btn btn-primary" data-dismiss="modal">Close</button>
+                                </div>
+                              </div>
+                      </div>
+                    </div>
+                    `
+                    
+                    var popup = createElementFromHTML(popup_html)
+                    document.body.appendChild(popup)
+                    popup.style.position = "absolute"
+                    popup.style.top = "0px !important" //window.scrollY + popup.getBoundingClientRect().top
+                    var dev = document.getElementById("dev")
+                    dev.appendChild(ul)
+
+                    var close = document.getElementById("close_button")
+                    close.onclick = function(){
+                        document.getElementById("a_temp").remove()
+                        document.getElementById("popup_temp").remove()
+                    }
+
+                    var a_html = '<a id="a_temp" data-toggle="modal" data-target="#popup_temp" href="#"></a>'
+                    a = createElementFromHTML(a_html)
+
+                    dropdown_list = document.getElementById("dropdown-list")
+                    dropdown_list.appendChild(a)
+                   
+                    windowscrollY = window.scrollY
+                    a.click()
+                    popup.style.position = "absolute"
+                    popup.style.top = windowscrollY + "px"
+            }
+
+        }
         body.appendChild(bodyRow)
         for (var j = 0; j < col_number ; j++){ // for each column
-            bodyItem = document.createElement("td")
+            var bodyItem = document.createElement("td")
             bodyItem.innerText = data[column_names[j]][i]
             if(highlight_rows.includes(i)){bodyItem.style.color = "red"; bodyItem.style.fontWeight = "bold";}
                 
@@ -73,3 +132,9 @@ function loadDataFromFile(data){
     else{next.style.visibility = "hidden"}
 }
 
+
+function createElementFromHTML(html_str){
+    const temp = document.createElement("div");
+    temp.innerHTML = html_str;
+    return temp.firstElementChild;
+}
