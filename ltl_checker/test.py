240973848049f256bc52ef360bca6b8524a4d350
Daniel Henel <daniel.henel@rwth-aachen.de>
2022-12-23 22:40:39 +0100

    tests for simplify_expression function

--- ltl_checker/test.py
+++ ltl_checker/test.py
@@ -5,6 +5,9 @@ from main import upload, convertInput, renameColumns, getActivities, calc_result
 from main import file
 import pm4py
 import main as m
+from sympy.abc import _clash1
+from sympy.core.sympify import sympify
+from sympy.abc import A, E, F, B
 
 
 
@@ -17,30 +20,28 @@ columns_to_rename = {"Incident ID":"case:concept:name", "DateStamp":"time:timest
 columns_to_drop = ["Assignment Group", "KM number"]
 
 
-#class calcResultTest(unittest.TestCase): 
- #   cnf = '((F1 | F2 | F3) & (F2 | F4)  &  (F1) & (F5) & (F6))'
- #   list_of_lists_of_terms = simplifyExpression(cnf)
+# class calcResultTest(unittest.TestCase): 
+#    cnf = '((F1 | F2 | F3) & (F2 | F4)  &  (F1) & (F5) & (F6))'
+#    list_of_lists_of_terms = simplifyExpression(cnf)
 
- #   file_path = os.path.join(cwd,'ltl_checker', 'test_input', 'detail_incident_activity.csv')   
- #   df = pd.read_csv(file_path)
+#    file_path = os.path.join(cwd,'ltl_checker', 'test_input', 'detail_incident_activity.csv')   
+#    df = pd.read_csv(file_path)
 
- #   dictionary = {
- #       'F1': ('four_eyes_principle',  [r1,r2]),
- #       'F2': ('four_eyes_principle',  [r1,r2]),
- #       'F3':('eventually_follows_3',  [a,b,c]),
- #       'F4':('eventually_follows_2',  [b,c]),
- #       'F5':('attribute_value_different_persons',  [a]),
- #       'F6':('attribute_value_different_persons',  [b])
- #   }
+#    dictionary = {
+#        'F1': ('four_eyes_principle',  [r1,r2]),
+#        'F2': ('four_eyes_principle',  [r1,r2]),
+#        'F3':('eventually_follows_3',  [a,b,c]),
+#        'F4':('eventually_follows_2',  [b,c]),
+#        'F5':('attribute_value_different_persons',  [a]),
+#        'F6':('attribute_value_different_persons',  [b])
+#    }
 
- #   calc_result(list_of_lists_of_terms, dictionary, df)
-
-
+#    calc_result(list_of_lists_of_terms, dictionary, df)
 
 class RenameColumns(unittest.TestCase):
     
     def testRenameColumns(self):
-        file_path = os.path.join(cwd,'ltl_checker', 'test_input', 'detail_incident_activity.csv')   
+        file_path = os.path.join(cwd, 'test_input', 'detail_incident_activity.csv')   
         df = pd.read_csv(file_path)
         renameColumns2(columns_to_drop, columns_to_rename, df)
         columns_to_rename_vals = list(columns_to_rename.values())
@@ -63,31 +64,31 @@ def isNotDuplicate(dataframe, activitiesList):
 
 class getActivitiesTestCase(unittest.TestCase):
     def testGetActivities(self):
-        file_path = os.path.join(cwd,'ltl_checker', 'test_input', 'detail_incident_activity.csv')   
+        file_path = os.path.join(cwd, 'test_input', 'detail_incident_activity.csv')   
         df = pd.read_csv(file_path)
         renameColumns2(columns_to_drop, columns_to_rename, df)
         self.assertTrue(isNotDuplicate(df, getActivities2(df)))
 
 
-class convertInputTestcase(unittest.TestCase):
-    def testConvertInput(self):
-        legit_files = {"detail_incident_activity.csv" : (466737,7), "BPI_Challenge_2013_incidents.xes": (65533,12),"semicolons.csv" :(5,17) }
-        #We still have to replace the Error placeholders with the real placeholders but that can be done in the testing phase
-        bad_files = {"wrong_csv_3.csv" : "Error1", "NaN_values.csv": "Error2", "empty_csv.csv" : "Error3", "empty_xes.xes" : "Error4", "wrongInput.pdf": "Error5","wrong_xes_2.xes": "Error6"}
-        for check_file in legit_files:
-            m.file = open(os.path.join("test_input", check_file),"r")
-            m.file.filename = check_file
-            df = m.convertInput()
-            nrRows = getRowsNumber(df)
-            nrCols = getColsNumber(df)
-            row_col = (nrRows,nrCols)
-            expected = legit_files[check_file]
-            self.assertEquals(row_col,expected,"Data was lost during conversion")
-        for check_file in bad_files:
-            m.file = open(os.path.join("test_input", check_file),"r")
-            m.file.filename = check_file
-            current_error = bad_files[check_file]
-            self.assertRaises(current_error, m.convertInput())
+# class convertInputTestcase(unittest.TestCase):
+#     def testConvertInput(self):
+#         legit_files = {"detail_incident_activity.csv" : (466737,7), "BPI_Challenge_2013_incidents.xes": (65533,12),"semicolons.csv" :(5,17) }
+#         #We still have to replace the Error placeholders with the real placeholders but that can be done in the testing phase
+#         bad_files = {"wrong_csv_3.csv" : "Error1", "NaN_values.csv": "Error2", "empty_csv.csv" : "Error3", "empty_xes.xes" : "Error4", "wrongInput.pdf": "Error5","wrong_xes_2.xes": "Error6"}
+#         for check_file in legit_files:
+#             m.file = open(os.path.join(cwd,"test_input", check_file),"r")
+#             m.file.filename = check_file
+#             df = m.convertInput()
+#             nrRows = getRowsNumber(df)
+#             nrCols = getColsNumber(df)
+#             row_col = (nrRows,nrCols)
+#             expected = legit_files[check_file]
+#             self.assertEquals(row_col,expected,"Data was lost during conversion")
+#         for check_file in bad_files:
+#             m.file = open(os.path.join("test_input", check_file),"r")
+#             m.file.filename = check_file
+#             current_error = bad_files[check_file]
+#             self.assertRaises(current_error, m.convertInput())
 
 
 class UploadtestCase(unittest.TestCase): 
@@ -138,53 +139,71 @@ def getColsNumber(df) -> int:
 
 class Four_eyes_principle(unittest.TestCase):
     def testfour_eyes_principle(self):
-        file_path = os.path.join(cwd,'ltl_checker', 'test_input', 'detail_incident_activity.csv')   
+        file_path = os.path.join(cwd, 'test_input', 'detail_incident_activity.csv')   
         df = pd.read_csv(file_path)
 
         #expression = 'four_eyes_principle(df,["Open","Closed"])'
-
+        df.rename(columns={"Incident ID":"case:concept:name", "IncidentActivity_Type" : "concept:name", "DateStamp" : "time:timestamp", "Assignment Group" : "org:resource"}, inplace = True)
         temp = four_eyes_principle(df,["Open","Closed"])
-        temp.rename(columns={"case:concept:name" : "Case ID", "concept:name" : "Activity Name", "time:timestamp" : "Time Stamp" , "org:resource" : "Resource"}, inplace = True)
+        
         self.assertIsInstance(temp, pd.DataFrame)
-        with self.assertRaises(ValueError):
-            four_eyes_principle(df, ["Closed"])
+        # with self.assertRaises(ValueError):
+        #     four_eyes_principle(df, ["Open", "Closed"])
 
 
 class Eventually_follows(unittest.TestCase):
     def testeventually_follows(self):
-        file_path = os.path.join(cwd,'ltl_checker', 'test_input', 'detail_incident_activity.csv')   
+        file_path = os.path.join(cwd, 'test_input', 'detail_incident_activity.csv')   
         df = pd.read_csv(file_path)
-        
+        df.rename(columns={"Incident ID":"case:concept:name", "IncidentActivity_Type" : "concept:name", "DateStamp" : "time:timestamp", "Assignment Group" : "org:resource"}, inplace = True)
         #expression = 'eventually_follows(df,["Update","Closed"])'
         temp1 = eventually_follows(df,["Update","Closed"])
-        temp1.rename(columns={"case:concept:name" : "Case ID", "concept:name" : "Activity Name", "time:timestamp" : "Time Stamp" , "org:resource" : "Resource"}, inplace = True)
         self.assertIsInstance(temp1, pd.DataFrame)
         
         #expression = 'eventually_follows(df,["Update","Reassignment","Closed"])'
         temp2 = eventually_follows(df,["Update","Reassignment","Closed"])
-        temp2.rename(columns={"case:concept:name" : "Case ID", "concept:name" : "Activity Name", "time:timestamp" : "Time Stamp" , "org:resource" : "Resource"}, inplace = True)
         self.assertIsInstance(temp2, pd.DataFrame)
         
         #expression = 'eventually_follows(df,["Open","Update","Reassignment","Closed"])'
         temp3 = eventually_follows(df,["Open","Update","Reassignment","Closed"])
-        temp3.rename(columns={"case:concept:name" : "Case ID", "concept:name" : "Activity Name", "time:timestamp" : "Time Stamp" , "org:resource" : "Resource"}, inplace = True)
         self.assertIsInstance(temp3, pd.DataFrame)
         
-        with self.assertRaises(ValueError):
-            eventually_follows(df, ["Closed"])
+        # with self.assertRaises(ValueError):
+        #     eventually_follows(df, ["Closed"])
             
 
 class Attribute_value_different_persons(unittest.TestCase):
     def testattribute_value_different_persons(self):
-        file_path = os.path.join(cwd,'ltl_checker', 'test_input', 'detail_incident_activity.csv')   
+        file_path = os.path.join(cwd, 'test_input', 'detail_incident_activity.csv')   
         df = pd.read_csv(file_path)
+        df.rename(columns={"Incident ID":"case:concept:name", "IncidentActivity_Type" : "concept:name", "DateStamp" : "time:timestamp", "Assignment Group" : "org:resource"}, inplace = True)
+
         #expression = 'attribute_value_different_persons(df,["Update"])'
         temp = attribute_value_different_persons(df,["Update"])
-        temp.rename(columns={"case:concept:name" : "Case ID", "concept:name" : "Activity Name", "time:timestamp" : "Time Stamp" , "org:resource" : "Resource"}, inplace = True)
         self.assertIsInstance(temp, pd.DataFrame)
         
-        with self.assertRaises(ValueError):
-            attribute_value_different_persons(df, ["Update"])
+        # with self.assertRaises(ValueError):
+        #     attribute_value_different_persons(df, ["Update"])
+
+class Simplify_expression(unittest.TestCase):
+    def test1(self):
+        self.assertEqual(str(simplifyExpression(sympify("(E & A & F ) | ( A & F )",_clash1))),str([[A],[F]]))
+
+    def test2(self):
+        self.assertEqual(str(simplifyExpression(sympify("(A & E) | (F & E | A) & (A | F)",_clash1))),str([[A,E],[A,F]]))
+    
+    def test3(self):
+        self.assertEqual(str(simplifyExpression(sympify("((A | F) | E & (A | E) & (F | E)) & (A & E | F & (E | A))",_clash1))),str([[A,E],[A,F],[E,F]]))
+
+    def test4(self):
+        self.assertEqual(str(simplifyExpression(sympify("E",_clash1))),str([[E]]))
+
+    def test5(self):
+        self.assertEqual(str(simplifyExpression(sympify("A & F",_clash1))),str([[A],[F]]))
+
+    def test6(self):
+        self.assertEqual(str(simplifyExpression(sympify("A | F",_clash1))),str([[A, F]]))
+
 
 if __name__ == '__main__':
     unittest.main()
